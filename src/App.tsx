e8db6113797b015a55bd1a3ed98ed31e5908e08a
Israel Campiotti <israelcampiotti@gmail.com>
2025-10-19 13:58:37 -0300

    update apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Game from "./Game";
@@ -7,17 +9,21 @@ import Stats from "./Stats";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const colorScheme = useColorScheme();
   return (
-    <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="Game"
-        screenOptions={{
-          headerShown: false
-        }}
-      >
-        <Stack.Screen name="Game" component={Game} />
-        <Stack.Screen name="Stats" component={Stats} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <StatusBar translucent backgroundColor="transparent" barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'} />
+      <NavigationContainer>
+        <Stack.Navigator 
+          initialRouteName="Game"
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name="Game" component={Game} />
+          <Stack.Screen name="Stats" component={Stats} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
